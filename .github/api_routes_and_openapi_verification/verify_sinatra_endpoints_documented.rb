474f9e03c8ff313ad4bb34be9942d5b69f5f6ff8
Gaurao Tarpe <gaurao@poshmark.com>
2024-08-13 15:48:05 -0500

    url prefix map used

--- .github/api_routes_and_openapi_verification/verify_sinatra_endpoints_documented.rb
+++ .github/api_routes_and_openapi_verification/verify_sinatra_endpoints_documented.rb
@@ -10,7 +10,7 @@ def sanitize_route(route_with_verb)
   nil
 end
 
-def find_sinatra_routes(diff_file_path, url_prefix)
+def find_sinatra_routes_v1(diff_file_path, url_prefix)
   # Define the HTTP verbs that correspond to Sinatra routes
   http_verbs = %w[get post put delete patch]
   routes = Set.new
@@ -37,7 +37,35 @@ def find_sinatra_routes(diff_file_path, url_prefix)
 end
 
 
-all_routes = find_sinatra_routes(ENV['DIFF_FILE_PATH'],'')
+def find_sinatra_routes_v2(diff_file_paths, app_file_url_prefix_map)
+  # Define the HTTP verbs that correspond to Sinatra routes
+  http_verbs = %w[get post put delete patch]
+  routes = Set.new
+  # get /meta/charts do get /meta/charts/:chart_id/edition do
+  # get /meta/charts | get /meta/charts/:chart_id/edition |
+  # Read the file line by line and check for HTTP verbs
+  diff_file_paths.each do |diff_file|
+    puts "Chnaged File: #{file}"
+    url_prefix = app_file_url_prefix_map.get(diff_file)
+    File.readlines(diff_file).each_with_index do |line, _index|
+      line = sanitize_route(line.strip)
+      split_routes = line.split('|')
+      split_routes.each do |verb_and_path|
+        # Check if the line starts with an HTTP verb followed by a space or '('
+        next unless http_verbs.any? { |verb| line.strip.start_with?("#{verb} ", "#{verb}(") }
+        route_with_verb = sanitize_route(verb_and_path.strip)
+        route_verb_split = route_with_verb.split
+        verb = route_verb_split[0] if route_with_verb && route_verb_split.length.positive?
+        route = route_verb_split[1] if route_with_verb && route_verb_split.length > 1
+        routes << "#{verb} #{url_prefix}#{route}" if route[0] == '/'
+      end
+    end
+  end
+  routes
+end
+
+map = {'README.md' => '/readme', 'CHANGELOG.md' => '/changelog'}
+all_routes = find_sinatra_routes_v2(ENV['DIFF_FILE_PATH'],map)
 puts "All routes:"
 all_routes.each do |route|
   puts route
